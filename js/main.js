cd1a66a126b508c27aaa2082dd84e991ff511eec
Vasiliy <cartario@yandex.ru>
2020-01-25 15:27:39 +0300

    структура карточки

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+document.querySelector('.map').classList.remove('map--faded');
+
+// var cards = {
+//   author: {avatar: []},
+//   offer: {
+//     title: [],
+//     address: [],
+//     price: [],
+//     type: [],
+//     rooms: [],
+//     guests: [],
+//     checkin: [],
+//     checkout: [],
+//     features: [],
+//     description: [],
+//     photos: []
+//   },
+//   location: {x: 1, y: 2},
+// };
