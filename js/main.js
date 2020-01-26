959ada66d063e4ef17277e9c90013605977f8ff0
Vasiliy <cartario@yandex.ru>
2020-01-26 14:25:58 +0300

    структура, рандомные коорд

--- js/main.js
+++ js/main.js
@@ -2,31 +2,51 @@
 
 document.querySelector('.map').classList.remove('map--faded');
 
-
-
-
-
-for (var i = 1; i <= 8; i++) {
-  var AUTHOR = {avatar: 'img/avatars/user0' + i + '.png'};
-}
+var AVATAR = [
+  'img/avatars/user01.png',
+  'img/avatars/user02.png',
+  'img/avatars/user03.png',
+  'img/avatars/user04.png',
+  'img/avatars/user05.png',
+  'img/avatars/user06.png',
+  'img/avatars/user07.png',
+  'img/avatars/user08.png'
+];
 
 var titles = ['Красивая уютная квартира',
-      'Большой загородный дом',
-      'Обычный серый дом',
-      'Огромный золотой дворец',
-      'Уютная семейная квартирка',
-      'Стандартный деревянный дом',
-      'Нестандартный дом из кирпича',
-      'Загородный коттедж'];
+  'Большой загородный дом',
+  'Обычный серый дом',
+  'Огромный золотой дворец',
+  'Уютная семейная квартирка',
+  'Стандартный деревянный дом',
+  'Нестандартный дом из кирпича',
+  'Загородный коттедж'];
+
+var description = ['Описание1', 'Описание2',
+  'Описание3',
+  'Описание4',
+  'Описание5',
+  'Описание6',
+  'Описание7',
+  'Описание8'];
+
+var type = ['palace', 'flat', 'house', 'bungalo'];
+
+var checkin = ['12:00', '13:00', '14:00',
+  '12:00', '13:00', '14:00', '12:00', '13:00'];
+
+var checkout = ['13:00', '14:00',
+  '12:00', '13:00', '14:00', '12:00', '13:00', '14:00'];
+
+var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner', 'elevator', 'conditioner'];
+
+var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 var prices = function () {
   return Math.floor(Math.random() * 1000);
 };
-
-var type = ['palace',
-      'flat',
-      'house',
-      'bungalo'];
 
 var room = function () {
   return Math.floor(Math.random() * 10);
@@ -36,18 +56,43 @@ var guests = function () {
   return Math.floor(Math.random() * 15);
 };
 
-var checkin = ['12:00', '13:00', '14:00'];
-
-var checkout = ['12:00', '13:00', '14:00'];
-
-var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-
-var AdObj = {
-  author: AUTHOR,
-  offer: {
-    title: titles,
-    price: prices,
-
-  },
-  location: {x:, y: }
+var getRandom = function (min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 };
+
+var locationX = [
+  getRandom (130, 630),
+  getRandom (130, 630),
+  getRandom (130, 630),
+  getRandom (130, 630),
+  getRandom (130, 630),
+  getRandom (130, 630),
+  getRandom (130, 630),
+  getRandom (130, 630)];
+
+var locationY = [
+  getRandom (130, 630),
+  getRandom (130, 630),
+  getRandom (130, 630),
+  getRandom (130, 630),
+  getRandom (130, 630),
+  getRandom (130, 630),
+  getRandom (130, 630),
+  getRandom (130, 630)];
+
+for (var i = 0; i < 9; i++) {
+  var adObj = {author: AVATAR[i],
+    title: titles[i],
+    type: type[i],
+    checkin: checkin[i],
+    checkout: checkout[i],
+    features: features[i],
+    price: prices(),
+    room: room(),
+    guests: guests(),
+    description: description[i],
+    photos: photos[i],
+    location: {x: locationX[i], y: locationY[i]}
+  };
+  console.log(adObj);
+}
