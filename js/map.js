a8709fd7ab062bc2339740b90d42ebaa0c930272
cartario <cartario@yandex.ru>
2020-02-17 12:45:05 +0300

    перемещение метки

--- js/map.js
+++ js/map.js
@@ -65,9 +65,46 @@
   };
   MainPin.addEventListener('keydown', onMainPinEnter);
 
+  // перетаскивание
+  MainPin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      MainPin.style.top = (MainPin.offsetTop - shift.y) + 'px';
+      MainPin.style.left = (MainPin.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
   window.map = {
     getMainPinCoord: getMainPinCoord,
-    map: map
+    map: map,
+    MainPin: MainPin
   };
 
 })();
