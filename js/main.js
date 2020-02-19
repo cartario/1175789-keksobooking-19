ec5f6180e425d3d261daa7b71c62bcac965eda54
cartario <cartario@yandex.ru>
2020-02-19 11:29:40 +0300

    убил магисеское число

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@
 
 (function () {
 
+  var ESC_KEYCODE = 27;
   // заполняет адрес
   var addressInput = document.querySelector('#address');
   addressInput.value = window.map.getMainPinCoord();
 
   window.main = {
-    addressInput: addressInput
+    addressInput: addressInput,
+    ESC_KEYCODE: ESC_KEYCODE
   };
 })();
