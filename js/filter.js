f830ff243bda404bffcc8db92d2821fe48f7cb2f
Vasiliy <cartario@yandex.ru>
2020-03-09 12:59:16 +0300

    фильтр по фичям, по цене, по комнатам, по гостям

--- js/filter.js
+++ js/filter.js
@@ -22,6 +22,19 @@
   var filter = document.querySelector('.map__filters');
   var filterItems = filter.querySelectorAll('select, input');
   var typeSelect = filter.querySelector('#housing-type');
+  var roomSelect = filter.querySelector('#housing-rooms');
+  var guestsSelect = filter.querySelector('#housing-guests');
+  var priceSelect = filter.querySelector('#housing-price');
+  var featuresSelect = filter.querySelector('#housing-features');
+
+  // задает массив цен
+  var PriceRange = {
+    ANY: {MIN: 0, MAX: Infinity},
+    LOW: {MIN: 0, MAX: 10000},
+    MIDDLE: {MIN: 10000, MAX: 50000},
+    HIGH: {MIN: 50000, MAX: Infinity}
+  };
+
 
   // создает пустые массивы для данных
   var data = [];
@@ -34,6 +47,7 @@
 
     // активирует форму фильтр
     activateFilter();
+
 
     //
     return pinsData.slice(0, PINS_LIMIT);
@@ -60,8 +74,9 @@
     // создает копию данных
     filteredData = data.slice(0);
 
+
     // запускает ф-ю фильтрации по типу выбранного жилья
-    filteredData = filteredData.filter(filtrationByType);
+    filteredData = filteredData.filter(filtrationByType).filter(filtrationByRoom).filter(filtrationByGuests).filter(filtrationByPrice).filter(filtrationByFeatures);
 
 
     // удаляет отрисованные ранее пины
@@ -76,9 +91,56 @@
 
   // фильтрует по типу жилья
   var filtrationByType = function (it) {
+
+    // возвращает any или совпадение it.offer.type с инпутом
     return typeSelect.value === 'any' || it.offer.type === typeSelect.value;
   };
 
+  var filtrationByRoom = function (it) {
+    return roomSelect.value === 'any' || it.offer.rooms == roomSelect.value;
+  };
+
+  var filtrationByGuests = function (it) {
+
+    return guestsSelect.value === 'any' || it.offer.guests == guestsSelect.value;
+
+  };
+
+  var filtrationByFeatures = function (it) {
+
+    // создает пустой массив для хранения выбранных чекбоксов
+    var features = [];
+
+    // находит выбранные
+    var checkedFeatures = featuresSelect.querySelectorAll('input:checked');
+
+    // каждый выбранный чекбокс записывает в массив
+    checkedFeatures.forEach(function (item) {
+      features.push(item.value);
+
+    });
+
+    // пробегает по каждому элементу из выбранных чекбоксов
+    return features.every(function (element) {
+
+      // возвращает сопвадения
+      return it.offer.features.includes(element);
+    });
+
+
+  };
+
+  var filtrationByPrice = function (it) {
+
+    // текущая выбранная категория цен
+    var filteringPrice = PriceRange[priceSelect.value.toUpperCase()];
+
+    // сравнивает текущую категорию с ценой
+    if (it.offer.price >= filteringPrice.MIN && it.offer.price <= filteringPrice.MAX) {
+      return it;
+    }
+
+  };
 
   window.filter = {
     activateFiltration: activateFiltration
