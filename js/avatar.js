174db1e47c00bd7eda6bcf4a2fa6e1f796fc28c8
Vasiliy <cartario@yandex.ru>
2020-03-12 15:12:16 +0300

    create module8-task2

---
+++ js/avatar.js
@@ -0,0 +1,14 @@
+'use strict';
+(function () {
+
+  // create module8-task2
+
+  var inputAvatar = document.querySelector('#avatar');
+  inputAvatar.setAttribute('accept', 'image/png, image/jpeg');
+
+
+  window.avatar = {
+
+  };
+
+})();
