435ba21297105b136235bc5ee11aab80ece7c66a
Vasiliy <cartario@yandex.ru>
2020-03-12 22:55:57 +0300

    загрузка фото

--- js/avatar.js
+++ js/avatar.js
@@ -3,19 +3,38 @@
 
   // create module8-task2
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var ImageParams = {
+    WIDTH: 70,
+    HEIGHT: 70
+  };
 
   var inputAvatar = document.querySelector('.ad-form__field input[type=file]');
 
 
   var inputPhoto = document.querySelector('#images');
   var previewAvatar = document.querySelector('.ad-form-header__preview img');
-  var previewPhoto = document.querySelector('.ad-form__photo');
+  var previewPhotoImage = document.createElement('img');
+  previewPhotoImage.width = ImageParams.WIDTH;
+  previewPhotoImage.height = ImageParams.HEIGHT;
+  var previewPhoto = document.querySelector('.ad-form__photo').appendChild(previewPhotoImage);
+
 
   inputAvatar.setAttribute('accept', 'image/png, image/jpeg');
   inputPhoto.setAttribute('accept', 'image/png, image/jpeg');
 
 
-  var onChangePic = function () {
+  var createReader = function (preview, file) {
+    var reader = new FileReader();
+
+    reader.addEventListener('load', function (evt) {
+      preview.src = evt.target.result;
+    });
+
+    reader.readAsDataURL(file);
+  };
+
+  var onChangeAvatar = function () {
+
     var file = inputAvatar.files[0];
     var fileName = file.name.toLowerCase();
 
@@ -25,24 +44,34 @@
     });
 
     if (matches) {
-
-      // читает файл
-      var reader = new FileReader();
-
-      reader.addEventListener('load', function () {
-        previewAvatar.src = reader.result;
-      });
-
-      reader.readAsDataURL(file);
+      createReader(previewAvatar, file);
     }
-    inputAvatar.removeEventListener('change', onChangePic);
+
+    inputAvatar.removeEventListener('change', onChangeAvatar);
   };
 
-  var loadPic = function (input) {
-    input.addEventListener('change', onChangePic);
+  var onChangePhoto = function () {
+    var file = inputPhoto.files[0];
+    var fileName = file.name.toLowerCase();
+
+    // соответствие
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      createReader(previewPhoto, file);
+    }
+
+    inputPhoto.removeEventListener('change', onChangePhoto);
   };
 
-  loadPic(inputAvatar);
+  var loadPic = function (input, func) {
+    input.addEventListener('change', func);
+  };
+
+  loadPic(inputAvatar, onChangeAvatar);
+  loadPic(inputPhoto, onChangePhoto);
 
 
   window.avatar = {
